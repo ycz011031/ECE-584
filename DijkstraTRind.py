565b3bd99ff449121199d199ab64fc4c90ca4d84
unknown <yz69@illinois.edu>
2025-02-17 23:26:46 -0600

    HW1 rev1.0

--- DijkstraTRind.py
+++ DijkstraTRind.py
@@ -27,7 +27,7 @@ def bounds(x_list):
     :param x_list: list of Z3 variables
     :return: Z3 Boolean expression bounding all variables
     """
-    return z3.BoolVal(True)  # TODO
+    return And(*[And(x >= 0, x < K) for x in x_list])  # TODO
 
 
 def has_token(x_list, j):
@@ -37,7 +37,18 @@ def has_token(x_list, j):
     :param j: index of P_j
     :return: Z3 Boolean expression that if true then P_j is holding the token
     """
-    return z3.BoolVal(False)  # TODO
+    token_holder = -1  # Default to no token holder
+    for i in range(len(x_list)):
+        if x_list[i] == (x_list[(i - 1) % len(x_list)] + 1) % K:
+            if token_holder == -1:
+                token_holder = i  # First valid token holder found
+            else:
+                return False  # More than one token holder, invalid state
+    
+    return z3.BoolVal(True) if token_holder == j else z3.BoolVal(False)  # Return True if P_j is the token holder
+
+
+  # TODO
 
 
 def legal_config(x_list):
@@ -58,8 +69,24 @@ def transition_relation(old_x_list, new_x_list):
     :param new_x_list: variables after transition
     :return: Z3 Boolean expression that when true means the old state can transit to the new state
     """
-    return z3.BoolVal(False)  # TODO
+    constraints = []
+    update_conditions = []
 
+    for i in range(N):
+        can_update = And(
+            has_token(old_x_list, i),  
+            new_x_list[i] == (old_x_list[i] + 1) % K  
+        )
+
+        constraints.append(Or(
+            new_x_list[i] == old_x_list[i],
+            can_update  
+        ))
+
+        update_conditions.append(can_update)
+
+
+    return And(*constraints, AtMost(*update_conditions, 1), AtLeast(*update_conditions, 1))
 
 def invariant(x_list):
     """
@@ -75,7 +102,7 @@ def prove(conjecture):
     # Do not change
     # Setting up solver
     s = Solver()
-    s.set("sat.cardinality.solver", True)  # Some options to speed up the solver
+    # s.set("sat.cardinality.solver", True)  # Some options to speed up the solver
 
     s.add(Not(conjecture))  # Check unsat of negation for checking validity
     result = s.check()
@@ -92,20 +119,23 @@ def main():
     # 1. Create a list of z3 Int variables called prestate which is [x[0], x[1], x[2],....,x[15]]
     # use the range() function in Python and the parameter N
     # do not hardcode the list explicitly
-    prestate = [Int("x[0]")]  # TODO
+    prestate = [Int(f"x[{i}]") for i in range(N)]  # TODO
     # 2. create a list of z3 Int variables called poststate which is [x'[0], x'[1], x'[2],....,x'[N-1]]
-    poststate = [Int("x'[0]")]  # TODO
+    poststate = [Int(f"x'[{i}]") for i in range(N)]  # TODO
 
     # Do not change
     # Defines init as prestate that is legal
-    init = legal_config(prestate)
+    init = And(legal_config(prestate), bounds(prestate))
 
     # 3. Write the base_case predicate using the Implies() function of z3
-    base_case = z3.BoolVal(True)  # TODO
+    base_case = Implies(init, invariant(prestate))  # TODO
 
     # 4. Write the induction step predicate using the Implies() function of z3
     # prestate and poststate and transition_relation
-    ind_case = z3.BoolVal(True)  # TODO
+    ind_case = Implies(
+        And(invariant(prestate), bounds(prestate), transition_relation(prestate, poststate)),
+        And(invariant(poststate), bounds(poststate))
+        )  # TODO
 
     # Do not change
     print("## Proving base case:")
